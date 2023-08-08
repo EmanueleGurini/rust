9684bd613a99c4624d2ab3d8f7a8d63a9f45480e
EmanueleGurini <emanuele.gurini89@gmail.com>
2023-08-08 08:26:12 +0200

    Add: hello_cargo exercise

---
+++ hello_cargo/target/debug/deps/hello_cargo-3318b7493dd54b72.d
@@ -0,0 +1,5 @@
+/Users/emanuelegurini/Documents/1-Projects/Developer/rust/hello_cargo/target/debug/deps/hello_cargo-3318b7493dd54b72: src/main.rs
+
+/Users/emanuelegurini/Documents/1-Projects/Developer/rust/hello_cargo/target/debug/deps/hello_cargo-3318b7493dd54b72.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/emanuelegurini/Documents/1-Projects/Developer/rust/hello_cargo/target/debug/hello_cargo: /Users/emanuelegurini/Documents/1-Projects/Developer/rust/hello_cargo/src/main.rs
